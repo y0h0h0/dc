bf593e827c43ee1df5ecf96640d4f8f63a502cfa
Tom Teaser <y0h0h0@yandex.ru>
2017-05-15 22:35:39 +0300

    go

--- getAdmitAd.php
+++ getAdmitAd.php
@@ -1,5 +1,29 @@
 <?
 
+/*
+$place - идентификатор площадки. В нашем случае 21082.
+$limit - количество товаров в выдаче.
+$offers - количество магазинов, предоставляющих товар.
+$query - поисковый запрос.
+*** Ip-адрес подставляется автоматически.
+
+Примеры использования:
+$galaxy = getAdmitAd('21082', 1, 3, 'galaxy s8'); // 1 товар, 3 магазина.
+$galaxy_list = getAdmitAd('21082', 6, 1, 'Samsung galaxy s8'); // 6 товаров по одному магазину для каждого.
+
+
+Возвращает массив
+
+[$i]['name'] - название товара
+[$i]['photo'] - url картинки
+[$i]['offers'][$e]['price'] -  цена
+[$i]['offers'][$e]['currency'] - валюта
+[$i]['offers'][$e]['shop'] - название магазина
+[$i]['offers'][$e]['rating'] - рейтинг от 0 до 100
+[$i]['offers'][$e]['url'] - ссылка на товар в магазине
+
+*/
+
 function getAdmitAd($place, $limit, $offers, $query) {
   $src = json_decode(@file_get_contents('http://widget.admitad.com/market/search?place='.$place.'&offers='.$offers.'&limit='.$limit.'&q='.$query.'&partner=m&ip='.$_SERVER['REMOTE_ADDR']), true);
 
@@ -22,7 +46,3 @@ function getAdmitAd($place, $limit, $offers, $query) {
   }
   return $data;
 }
-
-$galaxy = getAdmitAd('21082', 1, 3, 'galaxy s8'); // 1 товар, 3 магазина.
-
-$galaxy_list = getAdmitAd('21082', 6, 1, 'Samsung galaxy s8'); // 6 товаров по одному магазину для каждого.
